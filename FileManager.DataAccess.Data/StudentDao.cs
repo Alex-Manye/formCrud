e5eca217990cd665ec24a63345136511cf6c6417
alexmanye <alexmanye@gmail.com>
2020-02-28 16:14:09 +0100

    "first commit - structure"

---
+++ FileManager.DataAccess.Data/StudentDao.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using FileManager.Common.Layer;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileManager.DataAccess.Data
+{
+    public class StudentDao
+    {
+        public Student Add(Student student)
+        {
+            throw new NotImplementedException (
+                "The method is not yet implemented");
+        }
+    }
+}
